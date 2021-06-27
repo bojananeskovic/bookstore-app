318f2347974b5ef27bd3dd11efab5b046584c8d9
IT3-2017 <sofija.djordjevic98@gmail.com>
2021-06-27 20:48:24 +0200

    Database fixed

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,19 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDEmVIZzlrSFpLVrC3cDoBzg1W3I8U1d9M",
+  authDomain: "bookstore-app-d3013.firebaseapp.com",
+  projectId: "bookstore-app-d3013",
+  storageBucket: "bookstore-app-d3013.appspot.com",
+  messagingSenderId: "676611398407",
+  appId: "1:676611398407:web:1caa2fe7678519ad3343a2",
+  measurementId: "G-Z7P1EGQTT1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+/*const firebaseConfig = {
   apiKey: "AIzaSyBRxsDgkUTKwriBMi_48gvZl7CfX7kY1JY",
   authDomain: "bookstoreapp-b850c.firebaseapp.com",
   projectId: "bookstoreapp-b850c",
@@ -15,7 +28,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+firebase.analytics();*/
 
 // utils
 const db = firebase.firestore();
